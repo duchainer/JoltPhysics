4f5bd2796c84992c61d3237f590eb6d84c95889d
jrouwe <jrouwe@users.noreply.github.com>
2023-11-11 07:42:40 +0000

    deploy: 6bf0425394377cbd5ad343622c748a2224deac57

---
+++ navtreedata.js
@@ -0,0 +1,104 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "Jolt Physics", "index.html", [
+    [ "Architecture of Jolt Physics", "index.html", null ],
+    [ "Jolt Physics Samples", "md__docs__samples.html", null ],
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Variables", "namespacemembers_vars.html", null ],
+        [ "Typedefs", "namespacemembers_type.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ],
+        [ "Enumerator", "namespacemembers_eval.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ],
+        [ "Related Functions", "functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", "globals_func" ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"_a_a_b_b_tree_builder_8cpp.html",
+"_contact_listener_8h.html#ac6f5f34116f161cc13203803010c37c1",
+"_island_builder_8h.html",
+"_physics_material_simple_8cpp.html",
+"_shape_8h.html#aa332e0529e75b83aad91881c8d32551ba17843305369ea498e0b73fc07a427090",
+"_type_declarations_8cpp.html#a2b010525b68e5c2a57eb1025be64d97f",
+"class_body.html#a3c0a6198eb89412439dfcaef56eadade",
+"class_body_lock_interface_locking.html#a523d0b2ec0c63a14964ff67b407cf8dd",
+"class_cast_sphere_vs_triangles.html",
+"class_collision_group.html#abff4339a09c8ad38cc7aee4449d5ecaf",
+"class_convex_hull_builder2_d.html#a5827b465379b2d68b9bf2e6968565df5",
+"class_d_vec3.html#a95c9295adcf9cea2990be8d4c6b71891",
+"class_e_p_a_penetration_depth.html#acff8a6121c0896e3aabbe3a18019f201",
+"class_hinge_constraint_settings.html#ab9e2ce19ee6cde2921df8d3fba49d02e",
+"class_linear_curve.html#aa92a24bc4fff522ea56c5a0dadbf6ff4",
+"class_motorcycle_controller.html#a0fef8cc24d8f3afa9ec59a015fbd0639",
+"class_offset_center_of_mass_shape_settings.html#a641fb6993b220f935bed063a509acf38",
+"class_polyhedron_submerged_volume_calculator_1_1_point.html#a7d4332e216b4c207019734c00663b754",
+"class_ragdoll_settings_1_1_additional_constraint.html",
+"class_serializable_attribute.html#ab8ee3ad5a95fdf86aec0422cac3b7a42",
+"class_skeleton_pose.html#a067c1fa5353fa017050c331a43aeb63c",
+"class_sphere_shape.html#a9619a007d4bd51efef15792e464d8c0f",
+"class_swing_twist_constraint_settings.html#aa8a67f8a151ed609558127f83c3ddd6a",
+"class_two_body_constraint.html#a19ff8913100799b94972336188ed7c7f",
+"class_vector.html#a06ffa8aa3d40e7c93dde19725e77ff3a",
+"class_wheel_settings.html#a6958c6097a36cfe3e3a5ad4e8930ec1d",
+"namespace_closest_point.html#ad4bc500e8c3ea1bc2b2bc1a289408f1b",
+"struct_compound_shape_1_1_sub_shape.html#a3805d809932e3f0d94a3b87e297a47bc",
+"struct_ray_cast_t.html#a7eb5c3b516f95411ede61d7fd282bbd9"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
